34ce2cc1571fbbfb570908774ebe59847ee7c2ae
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-12-03 14:45:22 +0300

    full implementation for local party communicator

--- stalactite/communications.py
+++ stalactite/communications.py
@@ -7,7 +7,7 @@ from queue import Queue
 from threading import Thread
 from typing import List, Dict, Any, Optional
 
-from stalactite.base import Party, PartyDataTensor, PartyMaster, PartyMember, PartyCommunicator
+from stalactite.base import PartyMaster, PartyMember, PartyCommunicator
 
 logger = logging.getLogger(__name__)
 
@@ -63,10 +63,8 @@ class LocalThreadBasedPartyCommunicator(PartyCommunicator):
         return self._participants_info is not None
 
     def randezvous(self):
-        # todo cleaning the map (in threads being created)
         self._event_futures = dict()
-        # todo: correct thread initialization
-        self._participants_info ={
+        self._participants_info = {
             self.master.id: _MemberInfo(
                 queue=Queue(),
                 thread=Thread(name="master_event_loop", target=self._master_thread_func)
@@ -77,47 +75,45 @@ class LocalThreadBasedPartyCommunicator(PartyCommunicator):
                 ) for m in self.members)
         }
 
-    def send(self, send_to_id: str, method_name: str, **kwargs) -> Future:
+    def send(self, send_to_id: str, method_name: str, parent_id: Optional[str] = None, **kwargs) -> Future:
         self._check_if_ready()
 
         if send_to_id not in self._participants_info:
             raise ValueError(f"Unknown receiver: {send_to_id}")
 
-        event = _Event(id=str(uuid.uuid4()), parent_id=None, from_uid=self.participant_id,
+        event = _Event(id=str(uuid.uuid4()), parent_id=parent_id, from_uid=self.participant_id,
                        method_name=method_name, data=kwargs)
 
         return self._publish_message(event, send_to_id)
 
     def broadcast_send(self, method_name: str, mass_kwargs: Dict[str, Any], **kwargs) -> List[Future]:
         self._check_if_ready()
-        # todo logging of mass_kwargs here
+        logger.debug("Sending event (%s) for all members" % method_name)
 
         futures = []
 
-        for i, m in enumerate(self.members):
+        for m in self.members:
             args = mass_kwargs.get(m.id, None)
 
             if not args:
-                # todo: warning
+                logger.warning("No data to sent for member %s. Skipping it." % m.id)
                 continue
 
-            # todo: add correct from_uid
-            event = _Event(id=str(uuid.uuid4()), parent_id=None, from_uid='',
+            event = _Event(id=str(uuid.uuid4()), parent_id=None, from_uid=self.participant_id,
                            method_name=method_name, data={'data': args, **kwargs})
 
             future = self._publish_message(event, m.id)
-
-            # todo change message
-            logger.debug(f"PARTY: Sending  batch & rhs to member {i + 1}")
-
             futures.append(future)
 
         return futures
 
-    def _publish_message(self, event: _Event, receriver_id: str):
+    def _publish_message(self, event: _Event, receiver_id: str):
         future = Future()
         self._event_futures[event.id] = future
-        self._participants_info[receriver_id].queue.put(event)
+        self._participants_info[receiver_id].queue.put(event)
+
+        logger.debug(f"Sent to %s event %s" % (receiver_id, event))
+
         return future
 
     def _check_if_ready(self):
@@ -133,16 +129,17 @@ class LocalThreadBasedPartyCommunicator(PartyCommunicator):
 
             logger.debug("Received event %s" % event)
 
-            if event.method_name == _Methods.service_return_answer:
+            if event.method_name == _Methods.service_return_answer.value:
                 if event.parent_id not in self._event_futures:
                     raise ValueError(f"No awaiting future with if {event.parent_id}."
                                      f"(Event {event.id} from {event.from_uid})")
 
+                logger.debug("Marking future %s as finished by answer of event %s" % (event.parent_id, event.id))
                 future = self._event_futures.pop(event.parent_id)
                 future.set_result(event.data)
                 future.done()
-            elif event.method_name == _Methods.service_heartbeat:
-                logger.info("Received hartbeat: %s" % event.data)
+            elif event.method_name == _Methods.service_heartbeat.value:
+                logger.info("Received heartbeat from %s: %s" % (event.id, event.data))
             elif event.method_name == _Methods.finalize:
                 logger.info("Finalized")
                 break
@@ -151,5 +148,38 @@ class LocalThreadBasedPartyCommunicator(PartyCommunicator):
 
         logger.info("Master thread for master %s has finished" % self.master.id)
 
-    def _member_thread_func(self):
-        pass
+    def _member_thread_func(self, member: PartyMember):
+        logger.info("Member thread for member %s has started" % member.id)
+
+        supported_methods = [
+            _Methods.synchronize_uids.value,
+            _Methods.register_records_uids.value,
+            _Methods.initialize.value,
+            _Methods.finalize.value,
+            _Methods.update_weights.value,
+            _Methods.predict.value,
+            _Methods.update_predict.value
+        ]
+
+        not_found_methods = [method_name for method_name in supported_methods if hasattr(member, method_name)]
+        if len(not_found_methods) > 0:
+            raise RuntimeError(f"Found methods not supported by member {member.id}: {not_found_methods}")
+
+        while True:
+            event = self._participants_info[member.id].queue.get()
+
+            logger.debug("Received event %s" % event)
+
+            if event.method_name in supported_methods:
+                method = getattr(member, event.method_name)
+                result = method(**event.data)
+
+                self.send(send_to_id=event.from_uid, method_name=_Methods.service_return_answer.value,
+                          parent_id=event.id, result=result)
+
+                if event.method_name == _Methods.finalize.value:
+                    break
+            else:
+                raise ValueError(f"Unsupported method {event.method_name} (Event {event.id} from {event.from_uid})")
+
+        logger.info("Member thread for member %s has finished" % member.id)
