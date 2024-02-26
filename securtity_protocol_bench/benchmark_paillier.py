7a2a3380b1b4759f37ecd4666967600d6ab06259
zakharova-anastasiia <nastyazakharova.nz@gmail.com>
2024-02-26 21:36:46 +0400

    Benchmark he-frameworks

---
+++ securtity_protocol_bench/benchmark_paillier.py
@@ -0,0 +1,100 @@
+import mlflow
+import click
+import numpy as np
+
+from helpers import PailierSP, max_abs_diff, shape_to_name
+from constants import ARR_SIZES, PLAIN_ARR_SIZE
+
+
+@click.command
+@click.option(
+    "--mlflow-addr", type=str, required=False, help="MlFlow URI.", default='node3.bdcl:5555',
+)
+@click.option(
+    "-e", "--experiment", type=str, required=False, help="MlFlow experiment name.", default="benchmark-paillier"
+)
+@click.option("--key-n-length", type=int, required=False, help="Length of the generated key.", default=512)
+def main(mlflow_addr: str, experiment: str, key_n_length: int):
+    # 'node3.bdcl:5555'
+    mlflow.set_tracking_uri(f"http://{mlflow_addr}")
+    experiment = f"{experiment}-{key_n_length}"
+    mlflow.set_experiment(experiment)
+
+    threads = [20, 40]
+    precisions = [1e-8, 1e-15]
+
+    for n_threads in threads:
+        for precision in precisions:
+            for array_size in ARR_SIZES:
+                with mlflow.start_run():
+                    log_params = {
+                        "n_jobs": n_threads,
+                        "precision": precision,
+                        "array_size": shape_to_name(array_size)
+                    }
+                    mlflow.log_params(log_params)
+
+                    paillier_sp = PailierSP(
+                        precision=precision,
+                        n_jobs=n_threads,
+                        key_n_length=key_n_length
+                    )
+                    paillier_sp.initialize()
+
+                    print('Generating arrays')
+
+                    s1 = np.random.uniform(-1e+5, 1e+5, size=array_size)
+                    s2 = np.random.uniform(-1e+5, 1e+5, size=array_size)
+
+                    print('Encrypting arrays')
+                    s1_enc = paillier_sp.encrypt(s1, log=True)
+                    s2_enc = paillier_sp.encrypt(s2, log=False)
+
+                    print('Addition')
+                    sum_enc_s1s2 = paillier_sp.add_cypher_cypher(s1_enc, s2_enc)
+
+                    print('Decrypting arrays')
+                    sum_enc_s1s2_decr = paillier_sp.decrypt(sum_enc_s1s2, log=False)
+                    s1_enc_decr = paillier_sp.decrypt(s1_enc, log=True)
+                    s2_enc_decr = paillier_sp.decrypt(s2_enc, log=False)
+
+                    mlflow.log_metric(
+                        'all_close_decr_sum',
+                        np.allclose(sum_enc_s1s2_decr, s1 + s2, rtol=1e-05, atol=1e-08, equal_nan=False)
+                    )
+                    mlflow.log_metric(
+                        'max_abs_diff_decr_sum',
+                        max_abs_diff(sum_enc_s1s2_decr, s1 + s2)
+                    )
+
+                    mlflow.log_metric(
+                        'all_close_sum_decr',
+                        np.allclose(s1_enc_decr + s2_enc_decr, s1 + s2, rtol=1e-05, atol=1e-08, equal_nan=False)
+                    )
+                    mlflow.log_metric(
+                        'max_abs_diff_sum_decr',
+                        max_abs_diff(s1_enc_decr + s2_enc_decr, s1 + s2)
+                    )
+
+                    for plain_size in PLAIN_ARR_SIZE:
+                        plain_arr = np.random.uniform(-1e-1, 1e-1, size=(plain_size, array_size[0]))
+                        print('Multiplying')
+                        mult_enc_s1s2 = paillier_sp.multiply_plain_cypher(plain_arr, s1_enc)
+                        print('Decrypting arrays')
+                        mult_enc_s1_decr = paillier_sp.decrypt(mult_enc_s1s2, log=False)
+
+                        mult_s1 = np.dot(plain_arr, s1)
+
+                        mlflow.log_metric(
+                            f'close_decr_mult_{plain_size}',
+                            np.allclose(mult_s1, mult_enc_s1_decr, rtol=1e-05, atol=1e-08, equal_nan=False)
+                        )
+
+                        mlflow.log_metric(
+                            f'max_abs_diff_mult_{plain_size}',
+                            max_abs_diff(mult_s1, mult_enc_s1_decr)
+                        )
+
+
+if __name__ == '__main__':
+    main()
---
+++ securtity_protocol_bench/helpers.py
@@ -0,0 +1,121 @@
+import mlflow
+import numpy as np
+import tenseal as ts
+from phe import paillier
+
+from stalactite.ml.arbitered.security_protocols.arrays_phe import catchtime, ArrayEncryptor, ArrayDecryptor, \
+    matr_right_prod
+
+
+def max_abs_diff(arr1: np.ndarray, arr2: np.ndarray):
+    print(arr1[:2, :2])
+    print(arr2[:2, :2])
+    return np.max(np.abs(arr1 - arr2))
+
+def shape_to_name(arr_shape: tuple[int, int]):
+    return f"_{arr_shape[0]}x{arr_shape[1]}_"
+
+
+class TensealSP:
+    def __init__(self, poly_modulus_degree: int, coeff_mod_bit_sizes: list[int], global_scale_pow: int, n_threads: int):
+        self.poly_modulus_degree = poly_modulus_degree
+        self.coeff_mod_bit_sizes = coeff_mod_bit_sizes
+        self.global_scale = 2 ** global_scale_pow
+        self.n_threads = n_threads
+
+        self.context = None
+
+    def initialize(self):
+        context = ts.context(
+            ts.SCHEME_TYPE.CKKS,
+            poly_modulus_degree=self.poly_modulus_degree,
+            coeff_mod_bit_sizes=self.coeff_mod_bit_sizes,
+            n_threads=self.n_threads,
+        )
+        context.generate_galois_keys()
+        # context.generate_relin_keys()
+        # context.global_scale = 2 ** 40
+        context.global_scale = self.global_scale
+        self.context = context
+
+    def encrypt(self, array: np.ndarray, log: bool) -> ts.CKKSTensor:
+        with catchtime() as time_encryption:
+            arr_anc = ts.ckks_tensor(self.context, array)
+
+        if log:
+            mlflow.log_metric(f'time_encryption', time_encryption.time)
+        return arr_anc
+
+    def decrypt(self, arr_enc: ts.CKKSTensor, log: bool) -> np.ndarray:
+        with catchtime() as time_decryption:
+            array = np.array(arr_enc.decrypt().tolist())
+        if log:
+            mlflow.log_metric(f'time_decryption', time_decryption.time)
+        return array
+
+    def add_cypher_cypher(self, arr_enc1: ts.CKKSTensor, arr_enc2: ts.CKKSTensor) -> ts.CKKSTensor:
+        with catchtime() as time_addition:
+            addition = arr_enc1 + arr_enc2
+        mlflow.log_metric(f'time_addition', time_addition.time)
+        return addition
+
+    def multiply_plain_cypher(self, plain: np.ndarray, cypher: ts.CKKSTensor) -> ts.CKKSTensor:
+        # plain_enc = ts.ckks_tensor(self.context, plain)
+        desired_shape = (plain.shape[0], cypher.shape[1])
+
+        with catchtime() as time_multiplication:
+            # mult = plain_enc.dot(cypher)
+            mult = cypher.transpose().mm(plain.T).transpose()
+
+        assert tuple(mult.shape) == desired_shape, f'Something went wrong, {mult.shape}, {desired_shape}'
+        mlflow.log_metric(f'time_multiplication_{plain.shape[0]}', time_multiplication.time)
+        return mult
+
+
+class PailierSP:
+    def __init__(self, precision: float, n_jobs: int, key_n_length: int = 512):
+        self.precision = precision
+        self.n_jobs = n_jobs
+        self.key_n_length = key_n_length
+
+        self.array_encryptor = None
+        self.array_decryptor = None
+
+    def initialize(self):
+        public_key, private_key = paillier.generate_paillier_keypair(n_length=self.key_n_length)
+        array_encryptor = ArrayEncryptor(public_key, n_jobs=self.n_jobs, precision=self.precision)
+        array_decryptor = ArrayDecryptor(private_key, n_jobs=self.n_jobs)
+
+        self.array_encryptor = array_encryptor
+        self.array_decryptor = array_decryptor
+
+    def encrypt(self, array: np.ndarray, log: bool) -> np.ndarray:
+        with catchtime() as time_encryption:
+            arr_anc = self.array_encryptor.encrypt(array)
+
+        if log:
+            mlflow.log_metric(f'time_encryption', time_encryption.time)
+        return arr_anc
+
+    def decrypt(self, arr_enc: np.ndarray, log: bool) -> np.ndarray:
+        with catchtime() as time_decryption:
+            array = self.array_decryptor.decrypt(arr_enc)
+        if log:
+            mlflow.log_metric(f'time_decryption', time_decryption.time)
+        return array
+
+    def add_cypher_cypher(self, arr_enc1: np.ndarray, arr_enc2: np.ndarray) -> np.ndarray:
+        with catchtime() as time_addition:
+            addition = arr_enc1 + arr_enc2
+        mlflow.log_metric(f'time_addition', time_addition.time)
+        return addition
+
+    def multiply_plain_cypher(self, plain: np.ndarray, cypher: np.ndarray) -> np.ndarray:
+        desired_shape = (plain.shape[0], cypher.shape[1])
+
+        with catchtime() as time_multiplication:
+            mult = matr_right_prod(plain, cypher, n_jobs=self.n_jobs)
+
+        assert mult.shape == desired_shape, f'Something went wrong, {mult.shape}, {desired_shape}'
+        mlflow.log_metric(f'time_multiplication_{plain.shape[0]}', time_multiplication.time)
+        return mult
