62126d1db0f3d341459aa72afddcb3a8b554d6e9
AlexGrig <alex.grigorievskiy@gmail.com>
2024-04-21 14:13:54 +0300

    Added see to confings, prepare_sbol_smm from command line.

--- examples/utils/prepare_sbol_smm_ag.py
+++ examples/utils/prepare_sbol_smm_ag.py
@@ -30,7 +30,7 @@ def split_save_datasets(df, train_users, test_users, columns, postfix_sample, di
     )
 
 
-def load_data(data_dir_path: str, parts_num: int = 2, is_single: bool = False, sbol_only: bool = False):
+def load_data(data_dir_path: str, parts_num: int = 2, sbol_only: bool = False):
     sbol_path = os.path.join(data_dir_path, "sbol")
     smm_path = os.path.join(data_dir_path, "smm")
 
@@ -46,7 +46,7 @@ def load_data(data_dir_path: str, parts_num: int = 2, is_single: bool = False, s
         sbol_labels[["user_id"]], shuffle=True, random_state=seed, test_size=0.15
     )
 
-    if not is_single:
+    if parts_num != 1:
         logger.info("Save vfl dataset labels part...")
         split_save_datasets(df=sbol_labels, train_users=users_train, test_users=users_test,
                             columns=["user_id", "labels"], postfix_sample=sample, part_postfix="master_part",
@@ -63,7 +63,7 @@ def load_data(data_dir_path: str, parts_num: int = 2, is_single: bool = False, s
         lambda x: list(x), axis=1)
     sbol_user_features = sbol_user_features[["user_id", "features_part_0", "labels"]]
 
-    if not is_single:
+    if parts_num != 1:
         logger.info("Save vfl dataset part 0...")
         split_save_datasets(df=sbol_user_features, train_users=users_train, test_users=users_test,
                             columns=["user_id", "features_part_0"], postfix_sample=sample, part_postfix="part_0",
@@ -87,7 +87,7 @@ def load_data(data_dir_path: str, parts_num: int = 2, is_single: bool = False, s
     smm_user_factors = sbol_labels[["user_id"]].merge(smm_user_factors, on="user_id", how="inner")
     smm_user_factors.rename(columns={"user_factors": "features_part_1"}, inplace=True)
 
-    if not is_single:
+    if parts_num != 1:
         logger.info("Save vfl dataset part 1...")
         split_save_datasets(df=smm_user_factors, train_users=users_train, test_users=users_test,
                             columns=["user_id", "features_part_1"], postfix_sample=sample, part_postfix="part_1",
@@ -114,16 +114,21 @@ def load_data(data_dir_path: str, parts_num: int = 2, is_single: bool = False, s
                 df=single_df, train_users=users_train, test_users=users_test,
                 columns=["user_id", "features_part_0", "labels"], postfix_sample=sample, dir_name_postfix="_single",
                 data_dir_path=data_dir_path, part_postfix="part_0")
-
+            
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Command line params')
 
     parser.add_argument('--save_path', type=str, default='~/stalactite_data',
                         help='Path where the splitted data is saved to')
 
+    parser.add_argument('--parts_num', type=int, default=2,
+                        help='Number of parts the data is split to')
+    
+    parser.add_argument('--sbol_only', type=bool, default=False,
+                        help='Whether only sbol data is used.')
+    
     args = parser.parse_args()
     save_path = Path(args.save_path).absolute()
 
-    load_data(save_path)
-
+    load_data(data_dir_path=save_path, parts_num=args.parts_num, sbol_only=args.sbol_only)
     print(f"Sbol and Ssm data are saved to: {save_path}")
