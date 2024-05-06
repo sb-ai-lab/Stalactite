c65a6b25e6fc5781aedc3ffaa492f77e35299165
AlexGrig <alex.grigorievskiy@gmail.com>
2024-05-06 12:11:23 +0300

    Distributed experiment run master

--- examples/configs/test_ag/logreg-sbol-smm_run_master.sh
+++ examples/configs/test_ag/logreg-sbol-smm_run_master.sh
@@ -1,3 +1,5 @@
-ssh -t comp_cpu1 "cd /home/azureuser/data/code/vfl-benchmark && python3 -m poetry run stalactite master start \
+ssh -t comp_cpu1 'export PATH=\"/home/azureuser/.local/bin:$PATH\"; cd /home/azureuser/data/code/vfl-benchmark && poetry run stalactite master start \
     --detached \
-    --config-path examples/configs/test_ag/logreg-sbol-smm_cloud_master.yml"
+    --config-path examples/configs/test_ag/logreg-sbol-smm_cloud_master.yml'
+
+#ssh -t comp_cpu1 'export PATH=\"/home/azureuser/.local/bin:$PATH\"; cd /home/azureuser/data/code/vfl-benchmark && echo $PATH'
