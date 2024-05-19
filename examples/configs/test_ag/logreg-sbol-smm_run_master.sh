ea27bf856beacf9f38dc5dd98f2080e1c35d8030
AlexGrig <alex.grigorievskiy@gmail.com>
2024-05-19 23:03:17 +0300

    Chenged prerequisites path.

--- examples/configs/test_ag/logreg-sbol-smm_run_master.sh
+++ examples/configs/test_ag/logreg-sbol-smm_run_master.sh
@@ -1,5 +1,4 @@
 ssh -t comp_cpu1 'export PATH=\"/home/azureuser/.local/bin:$PATH\"; cd /home/azureuser/data/code/vfl-benchmark && poetry run stalactite master start \
-    --detached \
-    --config-path examples/configs/test_ag/logreg-sbol-smm_cloud_master.yml'
+    --detached --config-path ./examples/configs/test_ag/logreg-sbol-smm_cloud_master.yml'
 
 #ssh -t comp_cpu1 'export PATH=\"/home/azureuser/.local/bin:$PATH\"; cd /home/azureuser/data/code/vfl-benchmark && echo $PATH'
